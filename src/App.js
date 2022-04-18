6ce05a7f3fb5a38dccec9b8b8c2325d083adbc66
nitinmathie <nitinrajkumar502@gmail.com>
2022-04-18 12:15:15 +0530

    Counter with redux store with a maximum and minimum values. Todo: with TDD

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { connect } from 'react-redux';
 
-function App(){
-    const [counter, setcounter] = React.useState(0);
+class App extends React.Component{
+    // const [counter, setcounter] = React.useState(0);
+    state = { count: 0 ,maxCount:100, message:'Cannot go beyond 0 and 100'}
+    increment = () => {
+        this.props.dispatch({type : 'INCREMENT'})
+      }
+    
+      decrement = () => {
+        this.props.dispatch({type : 'DECREMENT'})
+      }
+    render()
+    {
     return (
         <div >
             <h1> This is counter app</h1>
-            <div id="counter-value">{counter}</div>
-            <button id="increment-btn" onClick={()=> setcounter(counter+1)}>Increment</button>
-            <button id="decrement-btn" onClick={()=>               
-                counter<=0?0:setcounter(counter-1)}>Decrement</button>
+            <h1>{this.props.message}</h1>
+            <button id="increment-btn" onClick={this.increment}>+</button>
+            <div id="counter-value">{this.props.count}</div>
+            <button id="decrement-btn" onClick={this.decrement}>-</button>
             
         </div>
 
-    )
+     )
+    }
 }
-export default App;
+function mapStateToProps(state){
+    return{
+         count: state.count ,maxCount:state.maxCount, message:state.message
+    };
+}
+export default connect(mapStateToProps)(App);
+// export default App;
+
+// ()=>{
+//     this.state.count<=0?this.setState({
+//              count: 0
+//                 }):this.setState({
+//              count: this.state.count - 1
+//                 });
+// }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js'
+import {Provider} from 'react-redux';
+import { createStore } from 'redux';
+const initialState ={
+    count: 0,
+    maxCount: 100,
+    message: 'Cannot go beyond 0 and 100'
+};
+function reducer(state= initialState, action) {
+    switch(action.type) {
+        case 'INCREMENT':
+          return {
+            count: state.count + 1>20?20:state.count + 1,
+            message:state.count>=20?'Reached maximum':'Can be incremented further'
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+          };
+        case 'DECREMENT':
+          return {
+            count: state.count - 1<0?0:state.count - 1,
+            message:state.count<=0?'Reached minimum':'Can be decremented further'
+          };
+        default:
+          return state;
+      }
+  }
+  
+  const store = createStore(reducer);
+// const store = createStore();
+const Counter = () => (
+    <Provider store ={store}>
+        <App/>
+    </Provider>
+);
+ReactDOM.render(<Counter />, document.getElementById("root"));
 
